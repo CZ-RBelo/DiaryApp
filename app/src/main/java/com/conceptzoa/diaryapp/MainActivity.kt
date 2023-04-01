0c9e9bd4058e3d6349d2cf9db907043d4663ed70
CZ-RBelo <jr.belo@hotmail.com>
2023-04-01 18:46:47 +0100

    Mongo DB Setup and Implement the Authentication Screen

--- app/src/main/java/com/conceptzoa/diaryapp/MainActivity.kt
+++ app/src/main/java/com/conceptzoa/diaryapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.conceptzoa.diaryapp.navigation.Screen
+import com.conceptzoa.diaryapp.navigation.SetupNavGraph
 import com.conceptzoa.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +15,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/conceptzoa/diaryapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.conceptzoa.diaryapp.util
+
+object Constants {
+
+    const val APP_ID = "mydiaryapp-lqdsg"
+    const val CLIENT_ID = "281284850266-7sd0jh5d8rde97g0qjjucsg7i7plupbc.apps.googleusercontent.com"
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
